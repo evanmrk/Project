63536aa001796b06c882e42406d0df4fa8fe4127
Evan  Tafonao <evanmarkustafonao@gmail.com>
2025-01-24 22:53:37 +0700

    Add files via upload

---
+++ Java/main.java
@@ -0,0 +1,16 @@
+// public class Main {
+//      public static void main(String []args){
+//         // System.out.println("Hello World");  
+//         Pokemon p1 = new Pokemon ();
+//         p1.name = "Evan";
+//         p1.level = 20;ll
+
+//         System.out.println(p1.name + " is level " + p1.level);
+//      }
+// }
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ Java/pokemon.java
@@ -0,0 +1,9 @@
+public class Pokemon{
+
+    String name;
+    int level;
+
+    void attack(){
+        System.out.print(name + " is attacking");
+    }
+}
